4bdee99f9d98da2a7729b55671e6edaf923bc33e
Harsha Gedda <geddaharsha@gmail.com>
2024-02-05 08:14:19 -0600

    add union code

--- struct.c
+++ struct.c
@@ -10,18 +10,32 @@ struct employee_t {
 	char lastname[64];
 	float income;
 	bool ismanager;
-}
+};
+
+
+union myunion_u {
+	int x;
+	char c;
+	short s;
+};
 
 int main() {
 
 
 	struct employee_t employees[MAX_EMPLOYEES];
-
 	for(int i = 0; i < MAX_EMPLOYEES; i++) {
 		employees[i].income = 0;
 		employees[i].ismanager = false;
 	}
 	
+	printf("10th Employees income: %.2f\n", employees[9].income);
+	
+	union myunion_u u;
+	u.x = 0xabcdef12;
+
+	printf("%hx %hhx\n", u.s, u.c);
+	printf("integer value: %x\n", u.x);
+
 	return 0;
 
 }
