0362fe67000b709c3daa15dc98048d9fad7e00f7
Harsha Gedda <geddaharsha@gmail.com>
2024-03-02 12:09:38 -0600

    create dummy db

--- files.c
+++ files.c
@@ -5,10 +5,20 @@
 #include <unistd.h>
 #include <string.h>
 
+
+struct database_header_t {
+	unsigned short version;
+	unsigned short employees;
+	unsigned int filesize;
+};
+
 int main(int argc, char *argv[]) {
 	/* 0 = stdin
 	 * 1 = stdout
 	 * 2 = stderr */
+	
+	struct database_header_t head = {0};
+	struct stat dbStat = {0};
 
 	if(argc != 2) {
 		printf("Usage: %s <filename>\n", argv[0]);
@@ -21,11 +31,33 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	char *mydata = "hello there file!\n";
-	write(fd, mydata, strlen(mydata));
 	
-	close(fd);
+	if(read(fd, &head, sizeof(head)) != sizeof(head)) {
+		perror("read");
+		close(fd);
+		return -1;
+	}
 
+	printf("DB Version: %u\n", head.version);
+	printf("DB Number of Employees: %u\n", head.employees);
+	printf("DB File Length: %u\n", head.filesize);
+	
+
+	if(fstat(fd, &dbStat) < 0) {
+		perror("stat");
+		close(fd);
+		return -1;
+	}
+
+	printf("DB File length, reported by stat: %lu\n", dbStat.st_size);
+	
+	if(dbStat.st_size != head.filesize) {
+		printf("GET OUTTA HERE HACKER!\n");
+		close(fd);
+		return -2;
+	}
+
+	close(fd);
 	return 0;
 
 }
